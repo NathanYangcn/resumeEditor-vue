495b88eca569f05dfa22e13ba721f8bd658bc437
NathanYangcn <nathanyang1991@163.com>
2017-07-16 08:25:10 +0800

    修改 passetsPublicPath 生成预览链接

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
-    assetsPublicPath: '/resume-editor/dist',
+    assetsPublicPath: '/resumeEditor-vue/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
